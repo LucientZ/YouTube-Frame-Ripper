8a40f16aa208bcfb849c92d7c13d13baaf95fd79
LucientZ <koblenzpoisiden@gmail.com>
2022-11-16 13:00:47 -0600

    [ADD] Added sharp_to_text() and list_to_char() basic functionality

--- index.js
+++ index.js
@@ -7,14 +7,51 @@ const url = "https://www.youtube.com/watch?v=amxDI97hMeQ";
 const frames = 1100;
 const targetWidth = 24;
 const targetHeight = 18;
-const filetype = "image"; // Options are 'image' and 'text' 
+const filetype = "text"; // Options are 'image' and 'text' 
+
+
+function list_to_char(tuple){
+    switch (tuple){
+        case (255,255,255,255):
+            return "▉";
+        case (0,255,255,255):
+            return "▟";
+        case (255,0,255,255):
+            return "▙";
+        case (255,255,0,255):
+            return "▜";
+        case (255,255,255,0):
+            return "▛";
+        default:
+            return "▉"
+    }
+}
+
 
 async function sharp_to_text(imgBuffer){
-    //stub
+    const frame = await sharp(imgBuffer).removeAlpha().raw().toBuffer();
+    let text_frame = [];
+
+    for(let i = 0; i < frame.length; i += targetWidth*6){
+        var row = "";
+        for(let j = i; j < targetWidth * 3; j += 6){
+            row += list_to_char((frame.at(j), frame.at(j+3), frame.at(j+targetWidth*3), frame.at(j+targetWidth*3 + 3)));
+        }
+        row += "\n";
+        console.log(row);
+        //text_frame.push(row);
+    }
+    
+    
 }
 
 
 async function main(){
+    if (targetHeight * targetWidth % 4 != 0 && filetype == "text"){
+        console.log("To output to text, area of frames in pixels must be a multiple of 4.");
+        return;
+    }
+
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
@@ -37,11 +74,12 @@ async function main(){
 
         if(filetype == "image"){
             await sharp(imgBuffer).png({pallete: true}).toFile(`frames/frame${i}.png`);
+            console.log(`Saved frames/frame${i}.png`);
         }
         else if(filetype == "text"){
             sharp_to_text(imgBuffer);
         }
-        console.log(`Parsed frame number ${i}`);
+        
 
 
     }
